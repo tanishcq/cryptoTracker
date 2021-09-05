246846db2501f9294fe5de75291445521d7d25ef
Tanishcq Mehta <tanishcqmehta@gmail.com>
2021-09-05 16:46:15 +0530

    fixed minor issues

--- src/components/anime.js
+++ src/components/anime.js
@@ -2,12 +2,28 @@ import React from 'react'
 import styled from 'styled-components';
 import {motion} from 'framer-motion';
 import '../App.css';
+import './anime.css';
+import './login.js';
+
 import BitcoinSvg from '../images/bit.svg';
 import EtherSvg from '../images/eth.svg';
 import AdaSvg from '../images/ada.svg';
 import DogeSvg from '../images/doge.svg';
-import './anime.css';
-import './login.js';
+import UsdtSvg from '../images/usdt.svg'
+import XrpSvg from '../images/xrp.svg'
+import BnbSvg from '../images/bnb.svg'
+import EtcSvg from '../images/etc.svg'
+import ShibSvg from '../images/shib.svg'
+import UniSvg from '../images/uni.svg'
+import DotSvg from '../images/dot.svg'
+
+
+const scrollToBottom = () =>{ 
+    window.scrollTo({ 
+      top: document.documentElement.scrollHeight - 1146, 
+      behavior: 'auto'
+    }); 
+  }; 
 
 const Section = styled.section`
     height: 100vh;
@@ -68,6 +84,15 @@ const Image = styled(motion.img)`
     max-height: 250px;
 `;
 
+const IMAGE = styled(motion.img)`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background: transparent;
+    max-width: 100px;
+    max-height: 100px;
+`;
+
 const ColumnnRight = styled.div`
     display: flex;
     justify-content: center;
@@ -92,6 +117,38 @@ const ColumnnRight = styled.div`
         bottom: 100px;
         right: 75px;
     }
+    ${Image}:nth-child(5){
+        bottom: 100px;
+        left: 75px;
+    }
+    ${Image}:nth-child(6){
+        bottom: 100px;
+        left: 125px;
+    }
+    ${Image}:nth-child(7){
+        bottom: 100px;
+        left: 175px;
+    }
+    ${Image}:nth-child(8){
+        bottom: 100px;
+        left: 530px;
+    }
+    ${Image}:nth-child(9){
+        bottom: 100px;
+        left: 355px;
+    }
+    ${Image}:nth-child(10){
+        bottom: 100px;
+        right: 275px;
+    }
+    ${Image}:nth-child(11){
+        bottom: 100px;
+        left: 175px;
+    }
+    ${Image}:nth-child(12){
+        bottom: 100px;
+        left: 470px;
+    }
 `;
 
 const anime = () => {
@@ -107,7 +164,7 @@ const anime = () => {
                 <ColumnnLeft>
                     <motion.h1 initial={{opacity:0}} animate={{opacity:1}} transition={{duration: 1}}>Welcome to <span id="logo1">crypto</span><span className="logo2">Rich</span></motion.h1>
                     <motion.p variants={fadeLeft} initial='hidden' animate='visible' transition={{duration: 2}}>Let's become <span className="logo2">rich</span> together</motion.p>
-                    <Button href="#reg-lock" whileHover={{ scale: 1.05, backgroundColor: '#ac32e4'}} whileTap={{scale: 0.95, backgroundColor: '#4801ff', border: 'none', color: '#ffcc00'}} initial={{opacity:0}} animate={{opacity:1, transition: {duration: 2}}}>Become <span className="logo2">Rich</span></Button>
+                    <Button onClick={scrollToBottom} whileHover={{ scale: 1.05, backgroundColor: '#ac32e4'}} whileTap={{scale: 0.95, backgroundColor: '#4801ff', border: 'none', color: '#ffcc00'}} initial={{opacity:0}} animate={{opacity:1, transition: {duration: 2}}}>Become <span className="logo2">Rich</span></Button>
                 </ColumnnLeft>
                 <ColumnnRight>
                     <Image src={BitcoinSvg} alt='coin' 
@@ -133,6 +190,54 @@ const anime = () => {
                     initial={{opacity: 0, y: 100}}
                     animate={{opacity: 1, y: 0, transition: {duration:1}}}
                     />
+                    {/* <IMAGE src={DotSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -500}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={XrpSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -550}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={XrpSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -250}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={ShibSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -350}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={BnbSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -125}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={UniSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: 100}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={EtcSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -110}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    />
+                    <IMAGE src={UsdtSvg} alt='coin' 
+                    whileTap={{scale:0.9}}
+                    drag={true}
+                    initial={{opacity: 1, y: -227}}
+                    animate={{opacity: 0, y: 9500, transition: {duration: 4}}}
+                    /> */}
                 </ColumnnRight>
             </Container>
         </Section>
--- src/components/login.js
+++ src/components/login.js
@@ -5,52 +5,53 @@ const login = () => {
     return (
         <>
         <script scr="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css"></script>
-            <div class="section">
-                <div class="container">
-                    <div class="row full-height justify-content-center">
-                        <div class="col-12 text-center align-self-center py-5">
-                            <div class="section pb-5 pt-5 pt-sm-2 text-center">
-                                <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
-                                <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
-                                <label for="reg-log"></label>
-                                <div class="card-3d-wrap mx-auto">
-                                    <div class="card-3d-wrapper">
-                                        <div class="card-front">
-                                            <div class="center-wrap">
-                                                <div class="section text-center">
-                                                    <h4 class="mb-4 pb-3">User Log In</h4>
-                                                    <div class="form-group">
-                                                        <input type="email" name="logemail" class="form-style" placeholder="Email" id="log_email" autocomplete="none" required/>
-                                                        <i class="input-icon uil uil-at"></i>
+        <div className="dummyDiv"></div>
+            <div className="section">
+                <div className="container">
+                    <div className="row full-height justify-content-center">
+                        <div className="col-12 text-center align-self-center py-5">
+                            <div className="section pb-5 pt-5 pt-sm-2 text-center">
+                                <h6 className="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+                                <input className="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+                                <label htmlFor="reg-log"></label>
+                                <div className="card-3d-wrap mx-auto">
+                                    <div className="card-3d-wrapper">
+                                        <div className="card-front">
+                                            <div className="center-wrap">
+                                                <div className="section text-center">
+                                                    <h4 className="mb-4 pb-3">User Log In</h4>
+                                                    <div className="form-group">
+                                                        <input type="email" name="logemail" className="form-style" placeholder="Email" id="log_email" autoComplete="none" required/>
+                                                        <i className="input-icon uil uil-at"></i>
                                                     </div>	
-                                                    <div class="form-group mt-2">
-                                                        <input type="password" name="logpass" class="form-style" placeholder="Password" id="log_pass" autocomplete="none" required/>
-                                                        <i class="input-icon uil uil-lock-alt"></i>
+                                                    <div className="form-group mt-2">
+                                                        <input type="password" name="logpass" className="form-style" placeholder="Password" id="log_pass" autoComplete="none" required/>
+                                                        <i className="input-icon uil uil-lock-alt"></i>
                                                     </div>
-                                                    <a href="#abc" class="btn mt-4" id="btn_login">submit</a>
-                                                    <p class="mb-0 mt-4 text-center">
-                                                        <a href="#abc" class="link">Forgot your password?</a>
+                                                    <a href="#abc" className="btn mt-4" id="btn_login">submit</a>
+                                                    <p className="mb-0 mt-4 text-center">
+                                                        <a href="#abc" className="link">Forgot your password?</a>
                                                     </p>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="card-back">
-                                            <div class="center-wrap">
-                                                <div class="section text-center">
-                                                    <h4 class="mb-4 pb-3">User Sign Up</h4>
-                                                    <div class="form-group">
-                                                        <input type="text" name="signname" class="form-style" placeholder="Name" id="sign_name" autocomplete="none" required/>
-                                                        <i class="input-icon uil uil-user"></i>
+                                        <div className="card-back">
+                                            <div className="center-wrap">
+                                                <div className="section text-center">
+                                                    <h4 className="mb-4 pb-3">User Sign Up</h4>
+                                                    <div className="form-group">
+                                                        <input type="text" name="signname" className="form-style" placeholder="Name" id="sign_name" autoComplete="none" required/>
+                                                        <i className="input-icon uil uil-user"></i>
                                                     </div>	
-                                                    <div class="form-group mt-2">
-                                                        <input type="email" name="signemail" class="form-style" placeholder="Email" id="sign_email" autocomplete="none" required/>
-                                                        <i class="input-icon uil uil-at"></i>
+                                                    <div className="form-group mt-2">
+                                                        <input type="email" name="signemail" className="form-style" placeholder="Email" id="sign_email" autoComplete="none" required/>
+                                                        <i className="input-icon uil uil-at"></i>
                                                     </div>	
-                                                    <div class="form-group mt-2">
-                                                        <input type="password" name="signpass" class="form-style" placeholder="Password" id="sign_pass" autocomplete="none" required/>
-                                                        <i class="input-icon uil uil-lock-alt"></i>
+                                                    <div className="form-group mt-2">
+                                                        <input type="password" name="signpass" className="form-style" placeholder="Password" id="sign_pass" autoComplete="none" required/>
+                                                        <i className="input-icon uil uil-lock-alt"></i>
                                                     </div>
-                                                    <a href="#abc" class="btn mt-4" id="btn_signup">submit</a>
+                                                    <a href="#abc" className="btn mt-4" id="btn_signup">submit</a>
                                                 </div>
                                             </div>
                                         </div>
